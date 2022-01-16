4094d927e5b21f0ed3a47d06623540c57f2b1160
byeonhyeok <bhrevol@gmail.com>
2022-01-16 20:27:28 +0900

    cli: add login command

--- src/afesta_tools/__main__.py
+++ src/afesta_tools/__main__.py
@@ -1,12 +1,90 @@
 """Command-line interface."""
+import asyncio
+from typing import Optional
+from typing import cast
+
 import click
 
+from .config import APP_NAME
+from .config import dump_credentials
+from .config import load_credentials
+from .exceptions import AfestaError
+from .exceptions import NoCredentialsError
+from .lpeg.client import FourDClient
+from .lpeg.credentials import BaseCredentials
+from .lpeg.credentials import FourDCredentials
 
-@click.command()
+
+@click.group()
 @click.version_option()
-def main() -> None:
+def cli() -> None:
     """Afesta Tools."""
 
 
+def _load_credentials() -> BaseCredentials:
+    """Try to load default credentials.
+
+    Will attempt to load creds from afesta-tools config or an existing
+    4D Media Player installation, in that order.
+
+    Returns:
+        Default credentials.
+    """
+    try:
+        return load_credentials()
+    except NoCredentialsError:
+        pass
+    return FourDCredentials.get_default()
+
+
+@cli.command()
+@click.option("-u", "--username", default=None, help="Afesta username.")
+@click.option("-p", "--password", default=None, help="Afesta password.")
+@click.option(
+    "-f", "--force", is_flag=True, default=False, help="Overwrite existing credentials."
+)
+def login(
+    username: Optional[str], password: Optional[str], force: bool
+) -> int:  # noqa: DAR101
+    """Login to Afesta and register afesta-tools as a new player.
+
+    If username and/or password are not specified, they will be prompted via the
+    command-line.
+
+    Login is not required if 4D Media Player is installed and the current
+    user has logged into 4D Media Player and registered it with an Afesta
+    account.
+
+    Note that afesta-tools only stores username and API tokens (password will
+    not be saved to disk).
+    """
+    if not force:
+        try:
+            creds = load_credentials()
+            click.echo(f"Already logged in as {creds.uid}")
+            return 0
+        except NoCredentialsError:
+            pass
+    if not username:
+        username = click.prompt("Afesta username")
+    if not password:
+        password = click.prompt("Afesta password", hide_input=True)
+    try:
+        username = cast(str, username)
+        password = cast(str, password)
+        creds = asyncio.run(_login(username, password))
+        dump_credentials(creds)
+        click.echo(f"Logged into Afesta as {creds.uid}")
+    except AfestaError as exc:
+        click.echo(f"Login failed: {exc}", err=True)
+        return 1
+    return 0
+
+
+async def _login(username: str, password: str) -> BaseCredentials:
+    async with FourDClient() as client:
+        return await client.register_player(username, password)
+
+
 if __name__ == "__main__":
-    main(prog_name="afesta-tools")  # pragma: no cover
+    cli(prog_name=APP_NAME)  # pragma: no cover
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,16 @@
 """Test cases for the __main__ module."""
+from pathlib import Path
+from typing import List
+from typing import Optional
+
 import pytest
 from click.testing import CliRunner
+from pytest_mock import MockerFixture
 
 from afesta_tools import __main__
+from afesta_tools.lpeg.client import FourDClient
+
+from .lpeg.test_credentials import TEST_CREDENTIALS
 
 
 @pytest.fixture
@@ -13,5 +21,49 @@ def runner() -> CliRunner:
 
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
+    result = runner.invoke(__main__.cli)
     assert result.exit_code == 0
+
+
+@pytest.mark.parametrize(
+    "args, input",
+    [
+        (None, "username\npassword\n"),
+        (["-u", "username"], "password\n"),
+        (["-p", "password"], "username\n"),
+        (["-u" "username", "-p", "password"], None),
+    ],
+)
+def test_login(
+    runner: CliRunner,
+    mocker: MockerFixture,
+    config_dir: Path,
+    args: Optional[List[str]],
+    input: Optional[str],
+) -> None:
+    """Should login with username/password."""
+    register_player = mocker.patch.object(
+        FourDClient, "register_player", return_value=TEST_CREDENTIALS
+    )
+    result = runner.invoke(__main__.login, args=args, input=input)
+    register_player.assert_called_with("username", "password")
+    assert f"Logged into Afesta as {TEST_CREDENTIALS.uid}" in result.output
+
+
+def test_already_logged_in(
+    runner: CliRunner, config_dir: Path, mocker: MockerFixture
+) -> None:
+    """Should not login unless --force is provided."""
+    from afesta_tools.config import dump_credentials
+
+    register_player = mocker.patch.object(
+        FourDClient, "register_player", return_value=TEST_CREDENTIALS
+    )
+    dump_credentials(TEST_CREDENTIALS)
+    result = runner.invoke(__main__.login)
+    assert f"Already logged in as {TEST_CREDENTIALS.uid}" in result.output
+    register_player.assert_not_called()
+
+    result = runner.invoke(__main__.login, ["-u", "username", "-p", "password", "-f"])
+    assert "Logged into Afesta" in result.output
+    register_player.assert_called_with("username", "password")
