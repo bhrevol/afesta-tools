4f35c9c7639b900eb932c2884ae91504c8ba7f6c
byeonhyeok <bhrevol@gmail.com>
2022-01-16 21:08:51 +0900

    cli: add basic dl command

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 """Global test fixtures."""
+import os
 from pathlib import Path
+from typing import Generator
 
 import pytest
 from pytest_mock import MockerFixture
@@ -12,3 +14,14 @@ def config_dir(tmp_path: Path, mocker: MockerFixture) -> Path:
     config_dir.mkdir()
     mocker.patch("platformdirs.user_config_dir", return_value=str(config_dir))
     return config_dir
+
+
+@pytest.fixture
+def wdir(tmp_path: Path) -> Generator[Path, None, None]:
+    """Fixture for generating a new working directory."""
+    working_dir = tmp_path / "wdir"
+    working_dir.mkdir()
+    cwd = os.getcwd()
+    os.chdir(working_dir)
+    yield working_dir
+    os.chdir(cwd)
